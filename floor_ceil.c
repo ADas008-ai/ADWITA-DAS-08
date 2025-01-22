a8bc0735d36a6405b33d50b7bfab3572cef47317
ADas008-ai <adwitadas2006@gmail.com>
2025-01-22 21:54:15 +0530

    Add files via upload

---
+++ floor_ceil.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a;
+	float b,c;
+	scanf("%f",&a);
+	b=floor(a);
+	c=ceil(a);
+	printf("the floor value and ceil value respectively is=%f%f",b,c);
+	return 0;
+}
+
