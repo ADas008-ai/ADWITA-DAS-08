34539960e5659d1b6b39a1875291c2d28d422cba
ADas008-ai <adwitadas2006@gmail.com>
2025-04-05 22:46:51 +0530

    Add files via upload

---
+++ structure_distance.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	struct dist
+	{
+		int ft;
+		int inch;
+	}d1,d2,d3;
+	d1.ft=5;
+	d2.ft=5;
+	d1.inch=8;
+	d2.inch=9;
+	d3.ft=d1.ft+d2.ft;
+	d3.inch=d1.inch+d2.inch;
+	if(d3.inch>12)
+	{
+		d3.inch=d3.inch-12;
+		d3.ft++;
+	}
+	printf("%d",d3.inch);
+	printf("%d",d3.ft);
+}
+
+
